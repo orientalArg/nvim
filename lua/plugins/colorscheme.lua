6c6247dbc8c8537a10d9ba7c678f2c7c8a930aad
Ignacio Joaquin Reynoso <orientalArg@outlook.com>
2025-09-01 17:34:49 -0300

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"alexanderjeurissen/lumiere.vim",
+		"craftzdog/solarized-osaka.nvim",
+		"shaunsingh/nord.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+	},
+}
